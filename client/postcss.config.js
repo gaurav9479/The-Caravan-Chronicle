ffffd1af6014c6c3b7bb79f41582b5714d2187ac
gaurav_ph <gauravshivmurat2@gmail.com>
2025-10-21 23:02:27 +0530

    chore(client): switch to Tailwind v3 and standard PostCSS config; add global utilities and dark-mode class support

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,9 +1,4 @@
-import tailwindcss from '@tailwindcss/postcss'
+import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
-export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-}
+export default { plugins: [tailwindcss(), autoprefixer()] }
